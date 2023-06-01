ffd30e7be67808c7554f7eb6c1eb1b3bfa0bfa30
NhlakaniphoSingular <nmsutwana@singular.ac.za>
2023-06-01 05:25:37 -0700

    linked.

---
+++ python-if_else_loops_functions/linked_lists.c
@@ -0,0 +1,26 @@
+#e
+    {
+      while (current->next != NULL)
+	current = current->next;
+      current->next = new;
+    }
+
+return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+  listint_t *current;
+
+  while (head != NULL)
+    {
+      current = head;
+      head = head->next;
+      free(current);
+    }
+}
